18a18db6451c342108e30b4ae5398848d4e397b0
Philip Breczinski <philip.breczinski@gmail.com>
2020-11-25 20:22:18 -0600

    add level door sprites

--- game/environment/LevelDoor.gd
+++ game/environment/LevelDoor.gd
@@ -2,6 +2,10 @@ extends StaticBody
 
 export var targetScene : String setget setTargetScene
 export var tooltip : String setget setTooltip
+export var tooltipClosed : String setget setTooltipClosed
+export var pieceNumber : int setget setPieceNumber
+
+onready var global = get_node("/root/Global")
 
 func setTargetScene(ts):
 	targetScene = ts
@@ -9,12 +13,19 @@ func setTargetScene(ts):
 func setTooltip(tt):
 	tooltip = tt
 
+func setTooltipClosed(tt):
+	tooltipClosed = tt
+
+func setPieceNumber(pn):
+	pieceNumber = pn
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 func use():
-	get_tree().change_scene(targetScene)
+	if global.havePiece(pieceNumber):
+		get_tree().change_scene(targetScene)
 
 func getTooltip():
 	return tooltip
