98a576152c1617ce4ff569695d1d5eae16b35ca6
Philip Breczinski <philip.breczinski@gmail.com>
2020-11-26 15:24:00 -0600

    level door updates

--- game/environment/LevelDoor.gd
+++ game/environment/LevelDoor.gd
@@ -1,31 +1,46 @@
 extends StaticBody
 
 export var targetScene : String setget setTargetScene
-export var tooltip : String setget setTooltip
-export var tooltipClosed : String setget setTooltipClosed
 export var pieceNumber : int setget setPieceNumber
 
 onready var global = get_node("/root/Global")
 
+onready var sprite = $Sprite3D
+
+const tooltips = [
+	"GHOULISH GRAVEYARD",
+	"GHOULISH GRAVEYARD\ncomplete",
+	"DUNGEON OF DOOM",
+	"DUNGEON OF DOOM\ncomplete",
+	"CURSED COLOSSEUM",
+	"CURSED COLOSSEUM\ncomplete",	
+]
+
+var tooltip : String
+var tooltipClosed : String
+
 func setTargetScene(ts):
 	targetScene = ts
-
-func setTooltip(tt):
-	tooltip = tt
-
-func setTooltipClosed(tt):
-	tooltipClosed = tt
 
 func setPieceNumber(pn):
 	pieceNumber = pn
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var frame = pieceNumber * 2
+	if havePiece():
+		frame += 1
+	sprite.frame = frame
 
 func use():
-	if global.havePiece(pieceNumber):
+	if havePiece():
 		get_tree().change_scene(targetScene)
 
 func getTooltip():
-	return tooltip
+	if havePiece():
+		return tooltips[pieceNumber*2+1]
+	else:
+		return tooltips[pieceNumber*2]
+
+func havePiece():
+	return global.havePiece(pieceNumber)
