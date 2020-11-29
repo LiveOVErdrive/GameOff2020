9a32743fa44fb0300ea68de15f9229ef6bb03b8a
Philip Breczinski <philip.breczinski@gmail.com>
2020-11-29 14:17:09 -0600

    add opening menu

--- game/Cutscenes/OpeningMenu.gd
+++ game/Cutscenes/OpeningMenu.gd
@@ -1,16 +1,10 @@
 extends Node2D
 
+onready var animationPlayer = $AnimationPlayer
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	animationPlayer.play("blink")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene("res://game/Cutscenes/Intro.tscn")
